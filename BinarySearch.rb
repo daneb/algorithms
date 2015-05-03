a2b447e2056276fddd5df8f379ce6ed17150682b
daneb <dane.balia@hetzner.co.za>
2015-05-03 21:42:36 +0200

    First commit

---
+++ BinarySearch.rb
@@ -0,0 +1,12 @@
+primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+
+def binarysearch(my_array, target)
+	
+	min = 0
+	max = primes.length - 1
+	
+	while(result != target)
+		
+	end
+
+end
