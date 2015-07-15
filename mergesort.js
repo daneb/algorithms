c7766282a3159721e89fa12008ce7a774b7d4013
daneb <dane.balia@hetzner.co.za>
2015-07-15 22:33:17 +0200

    Added call to mergesort from app.js

--- mergesort.js
+++ mergesort.js
@@ -1,75 +1,78 @@
-var println = function(message) {
-	console.log(message);
-}
+module.exports = function () {
 
-// Takes in an array that has two sorted subarrays,
-//  from [p..q] and [q+1..r], and merges the array
-var merge = function(array, p, q, r) {
-    var lowHalf = [];
-    var highHalf = [];
-
-    var k = p;
-    var i;
-    var j;
-    for (i = 0; k <= q; i++, k++) {
-        lowHalf[i] = array[k];
-    }
-    for (j = 0; k <= r; j++, k++) {
-        highHalf[j] = array[k];
+    this.println = function (message) {
+        console.log(message);
     }
 
-    k = p;
-    i = 0;
-    j = 0;
+
+    // Takes in an array that has two sorted subarrays,
+    //  from [p..q] and [q+1..r], and merges the array
+    this.merge = function (array, p, q, r) {
+        var lowHalf = [];
+        var highHalf = [];
+
+        var k = p;
+        var i;
+        var j;
+        for (i = 0; k <= q; i++ , k++) {
+            lowHalf[i] = array[k];
+        }
+        for (j = 0; k <= r; j++ , k++) {
+            highHalf[j] = array[k];
+        }
+
+        k = p;
+        i = 0;
+        j = 0;
     
-    // Repeatedly compare the lowest untaken element in
-    //  lowHalf with the lowest untaken element in highHalf
-    //  and copy the lower of the two back into array
-    while(i < (q - p) && j < (r - q))
-    {
-        if(lowHalf[i] < highHalf[j]) {
+        // Repeatedly compare the lowest untaken element in
+        //  lowHalf with the lowest untaken element in highHalf
+        //  and copy the lower of the two back into array
+        while (i < (q - p) && j < (r - q)) {
+            if (lowHalf[i] < highHalf[j]) {
+                array[k] = lowHalf[i];
+                i++;
+            }
+            else {
+                array[k] = highHalf[j];
+                j++;
+            }
+            k++;
+        }
+
+    
+        // Once one of lowHalf and highHalf has been fully copied
+        //  back into array, copy the remaining elements from the
+        //  other temporary array back into the array
+        this.println("What's in " + array);
+        this.println("LowHalf " + lowHalf);
+        this.println("HiHalf " + highHalf);
+        this.println("k i j " + k + " " + i + " " + j);
+
+        while (i < lowHalf.length) {
             array[k] = lowHalf[i];
             i++;
+            k++;
         }
-        else {
+
+        while (j < highHalf[j]) {
+            this.println("In here");
             array[k] = highHalf[j];
             j++;
+            k++;
         }
-        k++;
-    }
 
-    
-    // Once one of lowHalf and highHalf has been fully copied
-    //  back into array, copy the remaining elements from the
-    //  other temporary array back into the array
-    println("What's in " + array);
-    println("LowHalf " + lowHalf);
-    println("HiHalf " + highHalf);
-    println("k i j " + k + " " + i + " " + j);
-    
-    while(i < lowHalf.length)
-    {
-        array[k + 1]= lowHalf[i];
-        i++;
-        k++;
-    }
-    
-    while(j < highHalf[j])
-    {
-        println("In here");
-        array[k] = highHalf[j];
-        j++;
-        k++;
-    }
-    
-};
+    };
 
 
-var array = [3, 7, 12, 14, 2, 6, 9, 11];
-merge(array, 0,
-    Math.floor((0 + array.length-1) / 2),
-    array.length-1);
-println("Array after merging: " + array);
-//Program.assertEqual(array, [2, 3, 6, 7, 9, 11, 12, 14]);
+    // Takes in an array and recursively merge sorts it
+    this.mergeSort = function (array, p, r) {
+        if (r > p) {
+            var q = Math.floor((p + r) / 2);
+            this.mergeSort(array, p, q);
+            this.mergeSort(array, q + 1, r);
+            this.merge(array, p, q, r);
+        }
+    };
 
-
+}
