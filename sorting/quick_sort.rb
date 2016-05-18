7809afc34c590e316cb45a024c98d057d9c5d695
daneb <dane.balia@hetzner.co.za>
2016-05-18 21:16:58 +0200

    Reversed quick sort

--- sorting/quick_sort.rb
+++ sorting/quick_sort.rb
@@ -1,12 +1,13 @@
-# Amazing piece of code 
+# Amazing piece of code, tweaked a little
 # Thank you - http://rosettacode.org/wiki/Sorting_algorithms/Quicksort#Ruby
 # 
 module Sorting
   class QuickSort
     def quicksort(array)
+      byebug
       return array if array.length <= 1
-      pivot = array[0]
-      left, right = array[1..-1].partition { |x| x < pivot }
+      pivot = array[array.length - 1]
+      left, right = array[0..-2].partition { |x| x < pivot }
       quicksort(left) + [pivot] + quicksort(right)
     end
   end
--- spec/quick_sort_spec.rb
+++ spec/quick_sort_spec.rb
@@ -1,12 +1,13 @@
 describe Sorting::QuickSort do
   before :each do
-    @array = [ 3, 1, 4, 1, 5, 9, 2, 6, 5, 3 ]
+    #@array = [ 3, 1, 4, 1, 5, 9, 2, 6, 5, 3 ]
+    @array = [ 3, 7, 8, 5, 2, 1, 9, 5, 4 ]
     @qs = Sorting::QuickSort.new
   end
 
   context 'quick sort' do
     it 'should return an ordered array' do
-      expect(@qs.quicksort(@array)).to eq [1, 1, 2, 3, 3, 4, 5, 5, 6, 9]
+      expect(@qs.quicksort(@array)).to eq [1, 2, 3, 4, 5, 5, 7, 8, 9]
     end
   end
 end
