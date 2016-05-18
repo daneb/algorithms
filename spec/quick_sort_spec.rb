6e13f18061678d84d90fd03e3d5a2ab6d9d8d0fa
daneb <dane.balia@hetzner.co.za>
2016-05-18 20:48:33 +0200

    Added quick sort optimized

--- spec/quick_sort_spec.rb
+++ spec/quick_sort_spec.rb
@@ -1,26 +1,12 @@
 describe Sorting::QuickSort do
   before :each do
     @array = [ 3, 1, 4, 1, 5, 9, 2, 6, 5, 3 ]
-    @qs = Sorting::QuickSort.new(@array)
+    @qs = Sorting::QuickSort.new
   end
 
-  context 'partition' do
-    it 'should select a pivot' do
-      expect(@qs.pivot).to eq @array[0]
-    end
-
-    it 'should half elements less than pivot on left, and greater on right' do
-      @qs.partition
-      expect(@qs.array).to eq [2, 1, 1, 3, 5, 9, 4, 6, 5, 3]
-    end
-  end
-
-  context 'swap' do
-    it 'should be able to swap values when required' do
-      array = [1,2,3,4,5]
-      qs = Sorting::QuickSort.new(array)
-      qs.swap(0, 1)
-      expect(qs.array).to eq [2,1,3,4,5]
+  context 'quick sort' do
+    it 'should return an ordered array' do
+      expect(@qs.quicksort(@array)).to eq [1, 1, 2, 3, 3, 4, 5, 5, 6, 9]
     end
   end
 end
